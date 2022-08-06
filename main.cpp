0e3afe990a9b6ce31f078491af7b0b16f6dfb502
kaps <kaps@list.ru>
2022-08-06 11:33:44 +0300

    removed pause

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@ bool stationIsFree = true;
 std::mutex station_access;
 
 void train(const int travelTime, const std::string& trainName) {
+    station_access.lock();
     std::cout << "The train " << trainName << " has left. Travel time: " << travelTime << "." << std::endl;
+    station_access.unlock();
     std::this_thread::sleep_for(std::chrono::seconds(travelTime));
     station_access.lock();
-    stationIsFree = false;
     std::cout << "The train " << trainName << " arrived at the station." << std::endl;
     std::string command;
     do {
@@ -17,7 +18,6 @@ void train(const int travelTime, const std::string& trainName) {
         std::cin >> command;
     } while (command != "depart");
     std::cout << "The train " << trainName << " left the station." << std::endl;
-    stationIsFree = false;
     station_access.unlock();
 }
 
@@ -26,9 +26,7 @@ int main() {
     std::cout << "Enter time for train A, B, C (sec.):" << std::endl;
     std::cin >> timeA >> timeB >> timeC;
     std::thread trainA(train, timeA, "A");
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
     std::thread trainB(train, timeB, "B");
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
     std::thread trainC(train, timeC, "C");
     trainA.join();
     trainB.join();
